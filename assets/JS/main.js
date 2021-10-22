e8a7e0d9944bb8772664fcaf19e1b250c6432667
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-22 18:14:42 +0200

    secondo me può andare così, riguardo la lezione e vedo se riesco a semplificare

---
+++ assets/JS/main.js
@@ -0,0 +1,43 @@
+// devo creare una lista di membri del team  e ogni membro avrà nome ruolo e immagine
+
+//esempio: 
+//nome:Ben Green
+//role: ceo
+//image: https://picsum.photos/id/1025/300/300
+
+const team = 
+[
+    { 
+        "name" : ["fabio", "pacifici"],
+        "role" : "headteacher",
+        "image" : "https://picsum.photos/id/1025/300/300"
+
+    },
+
+    {
+        "name" : ["tiziano", "nicolai"],
+        "role" : "tutor",
+        "image" : "https://picsum.photos/id/1025/300/300"
+    },
+
+    {
+        "name" : ["fabiola", "gardin"],
+        "role" : "tutor",
+        "image" : "https://picsum.photos/id/1025/300/300"
+    },
+
+    {
+        "name" : ["andrea", "tosi"],
+        "role" : "student",
+        "image" : "https://picsum.photos/id/1025/300/300"
+    },
+
+    {
+        "name" : ["pablo", "pacheco"],
+        "role" : "student",
+        "image" : "https://picsum.photos/id/1025/300/300"
+    }
+
+]
+
+console.log(team);
